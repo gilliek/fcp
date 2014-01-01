2700689397be7fd95a7840205c8c37944bacec84
Kevin Gillieron <kevin.gillieron@epfl.ch>
2014-01-01 12:30:55 +0100

    add main source file

---
+++ bin/fcp.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+=begin
+  _____ ____ ____
+ |  ___/ ___|  _ \
+ | |_ | |   | |_) |
+ |  _|| |___|  __/
+ |_|   \____|_|
+
+	FCP stands for FTP Copy. It aims to be a simple CLI tool that performs copy
+	through FTP.
+
+	Version:
+		1.0.0
+
+	Author:
+		Kevin Gillieron <kevin.gillieron@gw-computing.net>
+
+	License:
+		BSD 3-clauses (see LICENSE file)
+=end
+
+begin
+
+end
