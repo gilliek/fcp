9bbd8d16565bc6c4567091609521fe4190f9c396
Kevin Gillieron <kevin.gillieron@epfl.ch>
2014-01-01 12:34:14 +0100

    add Logger

--- bin/fcp.rb
+++ bin/fcp.rb
@@ -19,6 +19,22 @@
 		BSD 3-clauses (see LICENSE file)
 =end
 
+class Logger
+	@@verbose_enabled = false
+
+	def self.enable_verbose()  ; @@verbose_enabled = true      ; end
+	def self.disable_verbose() ; @@verbose_enabled = false     ; end
+	def self.verbose(msg="")   ; puts msg if @@verbose_enabled ; end
+
+	def self.info(msg="") ; puts msg        ; end
+	def self.warn(msg="") ; STDERR.puts msg ; end
+
+	def self.err(msg="", status=1)
+		STDERR.puts msg
+		exit status
+	end
+end
+
 begin
 
 end
