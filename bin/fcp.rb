a1080de70c75500819c92ee4e224206ddf3ad79e
Kevin Gillieron <kevin.gillieron@epfl.ch>
2014-01-01 12:38:33 +0100

    add FCP module skeleton

--- bin/fcp.rb
+++ bin/fcp.rb
@@ -35,6 +35,48 @@ class Logger
 	end
 end
 
+module FCP
+	require 'net/ftp'
+
+	MODE_ASCII = 0 # ASCII copy
+	MODE_BIN   = 1 # Binary copy
+
+	DEFAULT_CONFIG_PATH = [ENV["HOME"] + "/.config/ftpconfig",
+												 ENV["HOME"] + "/.ftpconfig"]
+	DEFAULT_PORT        = "21"
+
+	# copy options
+	@@host      = nil                  # FTP host. Must contain protocol (ftp://)
+	@@user      = nil                  # user if login mode is not anonymous
+	@@password  = nil                  # password if login mode is not anonymous
+	@@port      = DEFAULT_PORT         # FTP port
+	@@recursive = false                # for copying directories
+	@@anonymous = false                # false if credentials are needed
+	@@mode      = MODE_BIN             # transfer mode: ASCII or binary
+	@@config    = DEFAULT_CONFIG_PATH  # list of possible path for config file
+	@@dir       = ""                   # default remote directory
+	@@safe      = false                # Safe mode (ask before overwriting).
+
+	# accessors
+	def user=(value)      ; @@user = value      ; end
+	def password=(value)  ; @@password = value  ; end
+	def port=(value)      ; @@port = value      ; end
+	def recursive=(value) ; @@recursive = value ; end
+	def anonymous=(value) ; @@anonymous = value ; end
+	def mode=(value)      ; @@mode = value      ; end
+	def config=(value)    ; @@config = value    ; end
+	def safe=(value)      ; @@safe = value      ; end
+
+	def cp_from_ftp(source, target)
+		# TODO
+	end
+
+	# copy a given array of files to a remote FTP server.
+	def cp_to_ftp(source, target)
+		# TODO
+	end
+end
+
 begin
 
 end
