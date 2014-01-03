eb65725e44ee23acafa8aa95d0f6c573bdfbe20b
Kevin Gillieron <kevin.gillieron@epfl.ch>
2014-01-03 16:18:00 +0100

    add setup and teardown for the mock ftp server tests

--- test/mock_ftp_server_test.rb
+++ test/mock_ftp_server_test.rb
@@ -1,1 +1,20 @@
-# TODO
+require_relative "mock_ftp_server"
+require_relative "test_utils"
+
+require "test/unit"
+require "net/ftp"
+
+class TestMockFTPServer < Test::Unit::TestCase
+	include TestUtils
+
+	def setup
+		@ftp_server    = MockFTPServer.new
+		@server_thread = Thread.new { @ftp_server.start }
+		@files         = []
+	end
+
+	def teardown
+		Thread.kill(@server_thread)
+		TestUtils::remove_files
+	end
+end
