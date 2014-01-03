bb5423b3e290737f55d0b78cc4e96dbd6506d22c
Kevin Gillieron <kevin.gillieron@epfl.ch>
2014-01-03 19:00:57 +0100

    change the way to remove all files in test_utils

--- test/test_utils.rb
+++ test/test_utils.rb
@@ -1,16 +1,15 @@
 require "fileutils"
 
 module TestUtils
-	@@files = []
-
 	def create_file(filepath="foobar", content="Hello, World!")
 		f = File.open(filepath, "w")
 		f.puts content
 		f.close
-		@@files = filepath
 	end
 
-	def remove_files
-		@@files.each { |f| FileUtils.rm(f) }
+	def clear_tmp
+		Dir["./tmp_server/*", "./tmp_client/*"].each do |f|
+			FileUtils.rm_r(f)
+		end
 	end
 end
