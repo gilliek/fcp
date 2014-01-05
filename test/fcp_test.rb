c2d13ce076fc4869454b7da971d4e922e7721f9e
Kevin Gillieron <kevin.gillieron@epfl.ch>
2014-01-05 16:35:07 +0100

    add tests for safe mode

--- test/fcp_test.rb
+++ test/fcp_test.rb
@@ -28,6 +28,8 @@ class TestFCP < Test::Unit::TestCase
 		test_simple_upload
 		test_config
 		test_recursive_copy
+		test_safe_copy_without_overwriting
+		test_safe_copy_with_overwriting
 	end
 
 	private
@@ -42,7 +44,7 @@ class TestFCP < Test::Unit::TestCase
 			remote_dir  = "test/tmp_server/"
 			remote_file = remote_dir + "/" + File.basename(file)
 
-			create_file(file)
+			create_file(file,  "FOO")
 
 			test_copy_helper("#{cmd} #{file} ftp://localhost:#{remote_dir}",
 				file, remote_file)
@@ -57,7 +59,7 @@ class TestFCP < Test::Unit::TestCase
 			remote_dir  = "test/tmp_server/"
 			remote_file = remote_dir + "/" + File.basename(file)
 
-			create_file(file)
+			create_file(file, "BAR")
 
 			test_copy_helper("#{cmd} #{file} ftp://local:", file, remote_file)
 		end
@@ -84,14 +86,47 @@ class TestFCP < Test::Unit::TestCase
 			assert(File.exists?(remote_dir))
 			assert(File.directory?(remote_dir))
 
-			check_file(file1, remote_file1)
-			check_file(file2, remote_file2)
+			assert(check_file(file1, remote_file1))
+			assert(check_file(file2, remote_file2))
+		end
+
+		# fcp -s -c test/fcpconfig test/tmp_client/foo ftp://local:
+		# test:
+		#		- safe copy without overwriting
+		def test_safe_copy_without_overwriting
+			# we assume that the file is already created by previous tests
+			cmd         = "./bin/fcp -s -c test/fcpconfig"
+			file        = "test/tmp_client/bar"
+			remote_file = "test/tmp_server/foo"
+			expected    = "already exists. Would you like to overwrite it ? (y/n) "
+
+			IO::popen("#{cmd} #{file} ftp://local:foo", "r+") do |io|
+				io.puts "n"
+				assert_equal(io.read, "'#{File.basename(remote_file)}' #{expected}")
+				assert(!check_file(file, remote_file))
+			end
+		end
+
+		# fcp -s -c test/fcpconfig test/tmp_client/foo ftp://local:
+		# test:
+		#		- safe copy with overwriting
+		def test_safe_copy_with_overwriting
+			# we assume that the file is already created by previous tests
+			cmd         = "./bin/fcp -s -c test/fcpconfig"
+			file        = "test/tmp_client/bar"
+			remote_file = "test/tmp_server/foo"
+			expected    = "already exists. Would you like to overwrite it ? (y/n) "
+			IO::popen("#{cmd} #{file} ftp://local:foo", "r+") do |io|
+				io.puts "y"
+				assert_equal(io.read, "'#{File.basename(remote_file)}' #{expected}")
+				assert(check_file(file, remote_file))
+			end
 		end
 
 		def test_copy_helper(cmd, file, remote_file)
 			io = IO::popen(cmd)
 			assert(io.readlines.join.empty?)
-			check_file(file, remote_file)
+			assert(check_file(file, remote_file))
 		end
 
 		def check_file(file, remote_file)
@@ -101,8 +136,9 @@ class TestFCP < Test::Unit::TestCase
 			source_content = File.open(file).readlines.join
 			target_content = File.open(remote_file).readlines.join
 
-			assert_equal(
-				Digest::MD5.digest(source_content),
-				Digest::MD5.digest(target_content))
+			source_digest = Digest::MD5.digest(source_content)
+			target_digest = Digest::MD5.digest(target_content)
+
+			return source_digest == target_digest
 		end
 end
